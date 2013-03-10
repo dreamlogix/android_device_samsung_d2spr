f6a981454c43349aeb3cce58337310c248f9c64c
Shareef Ali <shareefalis@cyanogenmod.org>
2013-03-10 17:11:22 -0400

    d2: fix parsing error
    Conflicts:
    	BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,5 +24,8 @@ TARGET_OTA_ASSERT_DEVICE := d2spr
 # include
 -include vendor/samsung/d2spr/BoardConfigVendor.mk
 
-# kernel
-TARGET_KERNEL_CONFIG := cyanogen_d2spr_defconfig
+# Assert
+TARGET_OTA_ASSERT_DEVICE := d2spr,d2spi
+
+# Kernel
+TARGET_KERNEL_CONFIG    := cyanogen_d2spr_defconfig
